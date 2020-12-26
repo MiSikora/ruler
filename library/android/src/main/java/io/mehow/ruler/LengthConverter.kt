61454ad39aa73a727d7a6c898d43af7f77ac3f4e
Michał Sikora <michalsikora90@gmail.com>
2020-12-26 23:33:29 +0100

    Add KDoc to LengthConverter and LengthFormatter

--- library/android/src/main/java/io/mehow/ruler/LengthConverter.kt
+++ library/android/src/main/java/io/mehow/ruler/LengthConverter.kt
@@ -2,6 +2,14 @@ package io.mehow.ruler
 
 import android.content.Context
 
+/**
+ * Converts one [Length] to another one. Useful if you need to process an input before it is displayed to a user.
+ * Can be automatically applied during data formatting by [installing][Ruler.addConverter] it into the [Ruler].
+ */
 public fun interface LengthConverter {
+  /**
+   * Maps an input [Length] to a one that should be formatted. Returns `null` if an input handling
+   * should be delegated to a different converter.
+   */
   public fun Length<*>.convert(context: Context): Length<*>?
 }
--- library/android/src/main/java/io/mehow/ruler/LengthFormatter.kt
+++ library/android/src/main/java/io/mehow/ruler/LengthFormatter.kt
@@ -2,6 +2,14 @@ package io.mehow.ruler
 
 import android.content.Context
 
+/**
+ * Formats [Length] to a displayable output. Can be automatically applied during data formatting
+ * by [installing][Ruler.addFormatter] it into the [Ruler].
+ */
 public fun interface LengthFormatter {
+  /**
+   * Formats an input [Length] to a human-friendly text using a [separator] between a numeric value and unit.
+   * Returns `null` if an input handling should be delegated to a different formatter.
+   */
   public fun Length<*>.format(context: Context, separator: String): String?
 }
